6b4ac0c8405f0cc79bb02c78f00f12860bc4c52b
Rachel Winefield <rahelywine@gmail.com>
2020-11-21 18:24:29 +0200

    Node & Express server

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import bodyparser from 'body-parser';
+import productRoute from './routes/productRoute';
+
+dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+const app = express();
+app.use(bodyparser.json());
+app.use('/api/products', productRoute);
+
+app.listen(5000, () => {console.log("Server started at http://localhost:5000")})
